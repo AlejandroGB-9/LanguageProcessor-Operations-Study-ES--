841b098f07374a43268890c21380c9d19e75dcb4
Alex <elAlubius@gmail.com>
2023-03-16 22:48:01 +0100

    Update final

--- drLL.c
+++ drLL.c
@@ -95,12 +95,6 @@ int ParseVariable () //V::=a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A
 	val = array[pos];	//Una vez obetenido el index se obtiene el valor de esa variable en el array
 	return val;
 }
-{
-	/*Esta funcion devuelve el valor de una variable en el array*/
-	int pos = SearchIndex(); //Primero se obtiene el index de la variable
-	int val = array[pos];	//Una vez obetenido el index se obtiene el valor de esa variable en el array
-	return val;
-}
 
 int ParseNumber() //N::=0|1|2|3|4|5|6|7|8|9....
 {
